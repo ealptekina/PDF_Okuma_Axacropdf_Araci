74b9ef5831ba77204b0276102cc6c7383e491584
Elif Alptekin <ealptekina@gmail.com>
2024-10-03 02:44:35 +0300

    Add project files.

---
+++ PDF_Okuma_Axacropdf_Araci/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace PDF_Okuma_Axacropdf_Araci
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                axAcroPDF1.LoadFile(ofd.FileName);
+
+            }
+        }
+    }
+}
